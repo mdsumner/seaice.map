2b47e5d9f6fa1fb524348be1274b34ab5c8e9413
Michael Sumner <mdsumner@gmail.com>
2023-10-31 22:13:49 +0000

    sentinel

--- data-raw/kml-underway.R
+++ data-raw/kml-underway.R
@@ -7,3 +7,6 @@ uwy <- tail(uwy, 30 * 24 * 60)
 uwy <- uwy[seq(1, nrow(uwy), by = 4), ]
 #uwy2 <- dplyr::arrange(uwy2, date_time_utc)
 try(trip::write_track_kml(rep("nuyina", nrow(uwy)), uwy$longitude, uwy$latitude, utc = uwy$date_time_utc, kml_file = "data-raw/nuyina.kmz"))
+
+uwy <- uwy[seq(1, nrow(uwy), by = 4), ]
+try(trip::write_track_kml(rep("nuyina", nrow(uwy)), uwy$longitude, uwy$latitude, utc = uwy$date_time_utc, kml_file = "data-raw/nuyina_x4.kmz"))
