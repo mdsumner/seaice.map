5ffcd089e5344309939a81966d1b79ecd781fdaa
Michael Sumner <mdsumner@gmail.com>
2025-11-11 21:46:24 +0000

    comments

--- _targets.R
+++ _targets.R
@@ -33,21 +33,33 @@ tar_option_set(
   format = "qs"
 )
 tar_assign({
+  ## differentiate current year (which is very fast to scan) from the full backlog
   thisyear <- as.integer(format(Sys.Date(), "%Y")) |> tar_target(cue = tar_cue(mode = "always"))
+
   ## backlog
   bl_years <- seq(1978, thisyear-1) |> tar_target()
+  ## bowerbird scans the source website for images for every year, we invalidate this now and then
   bl_configs <- mk_bowerbird_year(bl_years) |> tar_target(pattern  = map(bl_years))
   bl_files <- run_bowerbird_year(bl_configs) |> tar_target(pattern = map(bl_configs))
-  ## this year
+
+  ## files are daily, so we want this to update much more regularly
   files <- run_bowerbird_year(mk_bowerbird_year(thisyear)) |> tar_target(cue = tar_cue(mode = "always"))
+
+  ## now we have the full set, near-daily URLs, north and south hemisphere, since 1978
   icefiles <- rbind(bl_files, files) |> tar_target( )
+
+  ## set up as input -> output for GDAL to warp pairs of north+south to a single global image
   northsouth <- url_date(icefiles) |> pivot_wider(names_from = c(hemi), values_from = c(url)) |>
     mutate(outfile = sprintf("%s/%s/concentration_v4.0_%s.tif", rootdir, format(date, "%Y"), date))  |>
     group_by(date) |> tar_group() |> tar_target(iteration = "group")
+
+  ## run the warp, create output images on S3 storage
   images <- sea_ice_image(northsouth) |>
     tar_target(format = "fst_tbl", pattern = map(northsouth),
                cue = tar_cue(mode = "always"),
                resources = tar_resources(fst = tar_resources_fst(compress = 0)))
+
+  ## clean up, and create an index table of the sources
   sources <- transmute(images, date,
                         source = gsub("s3://", sprintf("/vsicurl/%s/", endpoint), outfile),
                         north, south) |> tar_target()
